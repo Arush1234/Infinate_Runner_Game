345a12b175e207794e0a538a88a39a704eba4b60
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2021-12-12 14:13:29 -0500

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -1,7 +1,7 @@
 class Pipe {
   constructor() {
     this.x = width;
-    this.w = 90;
+    this.w = 290;
     this.gap = 120;
     this.min_height = 100;
     this.max_height = height - this.min_height - this.gap;
@@ -11,14 +11,11 @@ class Pipe {
 
   show() {
     fill(255, 0, 0);
-    /* top pipe */
-    // rect(this.x, 0, this.w, this.top);
     image(pipeRevImg, this.x, 0, this.w, this.top);
     /* bottom pipe */
     fill(0, 255, 0);
     let height_b = height - this.gap - this.top;
     let y_b = height - height_b;
-    // rect(this.x, y_b, this.w, height_b);
     image(pipeImg, this.x, y_b, this.w, height_b);
   }
 
