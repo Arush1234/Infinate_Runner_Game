7e6f380952e8069bd13da668385b1905adb867ca
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2021-12-12 14:32:56 -0500

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -13,9 +13,9 @@ class Pipe {
     fill(255, 0, 0);
     image(pipeRevImg, this.x, 0, this.w, this.top);
     fill(0, 255, 0);
-    let height_b = height - this.gap - this.top;
-    let y_b = height - height_b;
-    image(pipeImg, this.x, y_b, this.w, height_b);
+    let heights = height - this.gap - this.top;
+    let y = height - heights;
+    image(pipeImg, this.x, y, this.w, heights);
   }
 
   offScreen() {
